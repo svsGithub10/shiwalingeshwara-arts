0869eba297c74b1b0474f87e474bf21c2dcda5cf
Shantveeresh Sheelavantar <shantaveereshvs@gmail.com>
2025-09-05 12:19:21 +0530

    feat: add home controller, logo, and base template

---
+++ arts/src/main/java/com/shivalingeshwara/arts/ShivalingeshwaraArtsApplication.java
@@ -0,0 +1,13 @@
+package com.shivalingeshwara.arts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShivalingeshwaraArtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShivalingeshwaraArtsApplication.class, args);
+	}
+
+}
---
+++ arts/src/test/java/com/shivalingeshwara/arts/ShivalingeshwaraArtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivalingeshwara.arts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShivalingeshwaraArtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
